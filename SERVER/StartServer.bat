76cd5e881eb3a8c9d71d3935e52e67cbfd377af3
arvinpenaacosta <117697006+arvinpenaacosta@users.noreply.github.com>
2025-01-15 19:29:33 +0800

    Update StartServer.bat

--- SERVER/StartServer.bat
+++ SERVER/StartServer.bat
@@ -1,1 +1,1 @@
-uvicorn 2025_InvServer:app --host 192.168.1.18 --port 8892 --reload --ssl-keyfile=certs/server_unencrypted.key --ssl-certfile=certs/server.crt
+uvicorn InventoryServer:app --host 192.168.1.18 --port 8892 --reload --ssl-keyfile=certs/server_unencrypted.key --ssl-certfile=certs/server.crt
